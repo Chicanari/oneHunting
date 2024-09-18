bfb7aa1ba6b6140dad1a7bf1ccc063c2317e1dd9
toriyama <chicanari@gmail.com>
2024-09-18 22:26:43 +0900

    UserRecordDTOを作成

--- oneHunting/src/main/java/dto/UserRecordDTO.java
+++ oneHunting/src/main/java/dto/UserRecordDTO.java
@@ -3,13 +3,50 @@ package dto;
 public class UserRecordDTO implements java.io.Serializable {
 	
 	//privateでフィールドを作成してください。
-
+	private String accountId;
+	private String accountPass;
+	private String accountName;
+	private String accountMail;
+	private String accountIcon;
+	
+	
 	
 	/**
-	 * プロフィールの表示、編集時に必要な情報を格納するDTO
+	 * 検索時に必要な情報を格納するDTO
 	 */
-	public UserRecordDTO() {
-		// TODO 自動生成されたコンストラクター・スタブ
+	public UserRecordDTO(String accountId,String accountPass,String accountName,String accountMail,String accountIcon) {
+		this.accountId = accountId;
+		this.accountPass = accountPass;
+		this.accountName = accountName;
+		this.accountMail = accountMail;
+		this.accountIcon = accountIcon;
 	}
 
+
+	//検索時に必要な各getter/setterメソッドの作成
+	public String getAccountId() {
+		return accountId;
+	}
+	
+	
+	public String getAccountPass() {
+		return accountPass;
+	}
+	
+	
+	public String getAccountName() {
+		return accountName;
+	}
+	
+	
+	public String getAccountMail() {
+		return accountMail;
+	}
+	
+	
+	public String getAccountIcon() {
+		return accountIcon;
+	}
+	
+	
 }
