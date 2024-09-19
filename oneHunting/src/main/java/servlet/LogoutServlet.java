ff1346a3e09b242a5d7bab961a3f44be022a7cb2
ユーザー名は任意 <>
2024-09-19 09:25:09 +0900

    ログアウトのマッピングを修正

--- oneHunting/src/main/java/servlet/LogoutServlet.java
+++ oneHunting/src/main/java/servlet/LogoutServlet.java
@@ -18,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
  * 
  */
 
-@WebServlet("/loout")
+@WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
