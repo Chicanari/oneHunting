13328c9743b3b4d42f757e0de3a08ed1088878c2
soejima774 <nanashi7431@gmai.com>
2024-09-19 14:03:57 +0900

    プロフィール画面の表示順にユーザープロフィール情報のプライベートフィールドを作成しました。 インスタンス化に必要なデータでフィールドを初期化するコンストラクタを追加しました。 各フィールドのgetter/setterメソッド作成しました。

--- oneHunting/src/main/java/dto/UserProfileDTO.java
+++ oneHunting/src/main/java/dto/UserProfileDTO.java
@@ -3,13 +3,24 @@ package dto;
 public class UserProfileDTO implements java.io.Serializable {
 	
 	//privateでフィールドを作成してください。
-
+	private String accountIcon;
+	private String accountName;
+	private String accountId;
+	private String accountKen;
+	private String accountIntroduction;
+	private String accountGoodPoint;
+	
 	
 	/**
-	 * 
+	 * プロフィール表示・編集で必要な情報を格納するDTO
 	 */
-	public UserProfileDTO() {
-		// TODO 自動生成されたコンストラクター・スタブ
+	public UserProfileDTO(String accountIcon,String accountName,String accountId,
+							String accountKen,String accountIntroduction,String accountGoodPoint) {
+		this.accountId = accountId;
+		this.accountName = accountName;
+		this.accountKen = accountKen;
+		this.accountIcon = accountIcon;
+		this.accountIntroduction = accountIntroduction;
+		this.accountGoodPoint = accountGoodPoint;
 	}
-
 }
