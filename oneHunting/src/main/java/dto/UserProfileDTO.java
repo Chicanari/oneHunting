dd08c70ea55c4fa20ce92e084d0c888241e6b0fe
soejima774 <nanashi7431@gmai.com>
2024-09-19 15:04:09 +0900

    プロフィール画面の表示順にユーザープロフィール情報のプライベートフィールドを作成しました。
    インスタンス化に必要なデータでフィールドを初期化するコンストラクタを追加しました。
    各フィールドのgetter/setterメソッド作成しました。

--- oneHunting/src/main/java/dto/UserProfileDTO.java
+++ oneHunting/src/main/java/dto/UserProfileDTO.java
@@ -12,7 +12,7 @@ public class UserProfileDTO implements java.io.Serializable {
 	
 	
 	/**
-	 * プロフィール表示・編集で必要な情報を格納するDTO
+	 * プロフィール表示で必要な情報を格納するDTO
 	 */
 	public UserProfileDTO(String accountIcon,String accountName,String accountId,
 							String accountKen,String accountIntroduction,String accountGoodPoint) {
@@ -23,4 +23,66 @@ public class UserProfileDTO implements java.io.Serializable {
 		this.accountIntroduction = accountIntroduction;
 		this.accountGoodPoint = accountGoodPoint;
 	}
+
+
+	public String getAccountIcon() {
+		return accountIcon;
+	}
+
+
+	public void setAccountIcon(String accountIcon) {
+		this.accountIcon = accountIcon;
+	}
+
+
+	public String getAccountName() {
+		return accountName;
+	}
+
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+
+	public String getAccountId() {
+		return accountId;
+	}
+
+
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+
+
+	public String getAccountKen() {
+		return accountKen;
+	}
+
+
+	public void setAccountKen(String accountKen) {
+		this.accountKen = accountKen;
+	}
+
+
+	public String getAccountIntroduction() {
+		return accountIntroduction;
+	}
+
+
+	public void setAccountIntroduction(String accountIntroduction) {
+		this.accountIntroduction = accountIntroduction;
+	}
+
+
+	public String getAccountGoodPoint() {
+		return accountGoodPoint;
+	}
+
+
+	public void setAccountGoodPoint(String accountGoodPoint) {
+		this.accountGoodPoint = accountGoodPoint;
+	}
+	
+	
 }
