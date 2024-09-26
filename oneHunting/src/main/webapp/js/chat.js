1665424b2b2290b65c16c728e919e81d9991ce64
toriyama <chicanari@gmail.com>
2024-09-26 20:38:51 +0900

    チャットを最新のものから表示させるようにJSを追加

--- oneHunting/src/main/webapp/js/chat.js
+++ oneHunting/src/main/webapp/js/chat.js
@@ -1,5 +1,12 @@
-/* hamburger */
 
+
+/* チャットの表示を最新のもの（下）から表示させる */
+window.onload = function() {
+  var container = document.querySelector('.main-container');
+  container.scrollTop = container.scrollHeight;
+};
+
+/* hamburger */
 {
     $(function(){
         $('.header__btn').on('click', function(){
